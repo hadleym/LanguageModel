6bb710755519ea9694d83396eee6289beec0e0f2
Mark Hadley <hadleym@email.arizona.edu>
2017-04-21 22:04:41 -0700

    Still needs to lemmatize the query

--- src/main/java/com/hadleym/languagemodel/App.java
+++ src/main/java/com/hadleym/languagemodel/App.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
@@ -15,58 +19,187 @@ import edu.stanford.nlp.process.PTBTokenizer;
 import edu.stanford.nlp.simple.*;
 
 public class App {
-	Sentence sent = new Sentence("Lucy is in the sky with diamonds");
+	public static final double LAMBDA = .5;
+	public static final boolean DEBUG = false;
+	public static final String FILENAME = "input.txt";
 
 	public static void main(String[] args) throws IOException {
-		lemmatize(args[0]);
-	}
-
-	public static void tokenize(String filename) throws IOException {
-		DocumentPreprocessor dp = new DocumentPreprocessor(filename);
-		for (List<HasWord> sentence : dp) {
-			System.out.println(sentence);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int collectionTotal = 0;
+		// String query = "information";
+		List<DocumentInfo> documentInfos = new ArrayList<>();
+		List<String> documents = getDocumentsFromFile(FILENAME);
+		for (String doc : documents) {
+			DocumentInfo di = lemmatize(doc);
+			documentInfos.add(di);
+			collectionTotal += di.size;
 		}
 
-		PTBTokenizer<CoreLabel> ptbt = new PTBTokenizer<>(new FileReader(filename), new CoreLabelTokenFactory(), "");
-		while (ptbt.hasNext()) {
-			CoreLabel label = ptbt.next();
-			System.out.println(label);
+		if (DEBUG)
+			System.out.println("Collection Total: " + collectionTotal);
+		// System.out.println("Query: " + query);
+		while (true) {
+			System.out.print("Enter phrase to query (or <ctl-c> to quit):");
+			String input = br.readLine();
+			String[] queryList = parseInput(input);
+			List<DocumentRank> documentRanks = new ArrayList<DocumentRank>();
+			for (DocumentInfo di : documentInfos) {
+				double value = 1;
+				for (int i = 0; i < queryList.length; i++) {
+					int collectionFrequency = getCollectionFrequency(documentInfos, queryList[i]);
+					value *= getDocumentValue(di, collectionFrequency, collectionTotal, queryList[i], LAMBDA);
+				}
+				documentRanks.add(new DocumentRank(di.name, value));
+			}
+
+			Collections.sort(documentRanks);
+			System.out.println("Sorted ranks");
+			for (DocumentRank dr : documentRanks) {
+				System.out.println(dr.toString());
+			}
+
 		}
 
 	}
 
-	public static void lemmatize(String filename) throws IOException {
+	public static String[] parseInput(String s) {
+		String[] returnStrings = s.split("\\s+");
+
+		return returnStrings;
+
+	}
+
+	public static double getDocumentValue(DocumentInfo di, int collectionFrequency, int collectionTotal, String query,
+			double lambda) {
+		double docValue = (double) di.getWordCount(query) / (double) di.size * lambda;
+		double collectionValue = ((double) collectionFrequency) / (double) collectionTotal * (1 - lambda);
+		return docValue + collectionValue;
+	}
+
+	public static int getDocumentTotal(HashMap<String, Integer> map) {
+		int total = 0;
+		for (String key : map.keySet()) {
+			total += map.get(key);
+		}
+		return total;
+	}
+
+	public static List<String> getDocumentsFromFile(String filename) throws IOException {
+		List<String> documents = new ArrayList<String>();
 		Scanner sc = new Scanner(new File(filename));
 		while (sc.hasNext()) {
-			String line = sc.nextLine();
-			ParseDocument pd = new ParseDocument(line);
-			Document doc = new Document(pd.line);
-			for (Sentence sent : doc.sentences()) {
-				System.out.println(sent.parse());
-				List<String> words = sent.words();
-				for (int i = 0; i < words.size(); i++){
-					System.out.println("Word[" + i + "]: " + words.get(i));
-				}
+			documents.add(sc.nextLine());
+		}
+		return documents;
+	}
 
-				List<String> lemmas = sent.lemmas();
-				for (int i = 0; i < lemmas.size(); i++) {
-					String w = lemmas.get(i);
-					if (Character.isLetter(w.charAt(0)))
+	public static int getCollectionFrequency(List<DocumentInfo> documentInfos, String word) {
+		int count = 0;
+		for (DocumentInfo di : documentInfos) {
+			count += di.getWordCount(word);
+		}
+		return count;
+	}
+
+	/*
+	 * public static void tokenize(String filename) throws IOException {
+	 * DocumentPreprocessor dp = new DocumentPreprocessor(filename); for
+	 * (List<HasWord> sentence : dp) { System.out.println(sentence); }
+	 * 
+	 * PTBTokenizer<CoreLabel> ptbt = new PTBTokenizer<>(new
+	 * FileReader(filename), new CoreLabelTokenFactory(), ""); while
+	 * (ptbt.hasNext()) { CoreLabel label = ptbt.next();
+	 * System.out.println(label); }
+	 * 
+	 * }
+	 */
+	public static DocumentInfo lemmatize(String line) {
+		String name;
+		HashMap<String, Integer> map = new HashMap<>();
+		ParseDocument pd = new ParseDocument(line);
+		Document doc = new Document(pd.line);
+		name = pd.name;
+		for (Sentence sent : doc.sentences()) {
+			if (DEBUG)
+				System.out.println(sent.parse());
+			List<String> words = sent.words();
+			for (int i = 0; i < words.size(); i++) {
+				if (DEBUG)
+					System.out.println("Word[" + i + "]: " + words.get(i));
+			}
+
+			List<String> lemmas = sent.lemmas();
+			for (int i = 0; i < lemmas.size(); i++) {
+				String w = lemmas.get(i);
+				if (Character.isLetter(w.charAt(0))) {
+					if (DEBUG) {
 						System.out.println("Lemma[" + i + "]: " + lemmas.get(i));
+					}
+					if (map.containsKey(w)) {
+						map.put(w, map.get(w) + 1);
+					} else {
+						map.put(w, 1);
+					}
 				}
 			}
 		}
-
+		return new DocumentInfo(map, name);
 	}
-	
-
 }
 
-class ParseDocument{
-	public String name; 
+class DocumentInfo {
+	public String name;
+	public HashMap<String, Integer> map;
+	public int size;
+
+	public DocumentInfo(HashMap<String, Integer> m, String n) {
+		name = n;
+		map = m;
+		size = 0;
+		for (String key : map.keySet()) {
+			size += map.get(key);
+		}
+	}
+
+	public int getWordCount(String w) {
+		if (map.containsKey(w)) {
+			return map.get(w);
+		} else {
+			return 0;
+		}
+	}
+}
+
+class DocumentRank implements Comparable<DocumentRank> {
+	public String name;
+	public double rank;
+
+	public DocumentRank(String n, double r) {
+		name = n;
+		rank = r;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s: %4f", name, rank);
+	}
+
+	@Override
+	public int compareTo(DocumentRank o) {
+		if (o.rank > rank)
+			return 1;
+		if (o.rank == rank)
+			return 0;
+		return -1;
+	}
+}
+
+class ParseDocument {
+	public String name;
 	public String line;
-	public ParseDocument(String line){
+
+	public ParseDocument(String line) {
 		name = line.substring(0, line.indexOf(":"));
-		this.line = line.substring(line.indexOf(":")+1, line.length());
+		this.line = line.substring(line.indexOf(":") + 1, line.length());
 	}
 }
